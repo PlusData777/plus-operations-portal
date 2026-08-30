cab4bed9e4d6cc56d4b145f4dfc582bff9fa01a4
PlusData777 <dataplus.org@gmail.com>
2026-08-30 16:50:05 +0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,18 +8,8 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          navy: "#1b365d",
-          "navy-dark": "#122440",
-          yellow: "#fad207",
-          rust: "#c65a28",
-          ochre: "#e59a24",
-          red: "#b82626",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
+export default config;
